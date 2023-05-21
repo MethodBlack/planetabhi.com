ac726e0480d951caafb1c190c10f44ad18795cb3
Abhimanyu Rana <abhimanyu.rana@postman.com>
2023-05-21 20:10:40 +0530

    setup

---
+++ main.js
@@ -0,0 +1,54 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+<div class="centered-container">
+
+<header>
+  <div class="container">
+    <h1 class="title">abhi</h1>
+  </div>
+</header>
+
+<div id="particles"></div>
+<div id="particles2"></div>
+
+<section class="body">
+  <div class="container">
+
+    <div class="flex">
+      <div class="col-3">
+          <h3 class="section-title">Side Projects</h3>
+          <ul class="links">
+          <li><a href="https://dimerapp.com/">Dimer App</a></li>         
+          <li><a href="https://new-ui.com/">New UI</a> &#10024;</li>
+          <li><a href="https://remote-india.com/">Remote India</a></li> 
+          <li><a href="https://riseofmachine.com/">Rise of Machine</a></li>  
+          <li><a href="https://saaslanding.page/">SaaS Landing Pages</a></li>
+          <li><a href="https://sargamcolors.com/">Sargam Colors</a></li> 
+          <li><a href="https://sargam.design/">Sargam Design</a></li>                                              
+          <li><a href="https://sargamicons.com/">Sargam Icons</a></li>
+          <li><a href="https://yourvedas.com/">Your Vedas</a></li>
+        </ul>
+      </div>
+      
+      <div class="col-3">
+      <h3 class="section-title">Social</h3>
+          <ul class="links">
+          <li><a href="https://github.com/planetabhi">GitHub</a></li>    
+          <li><a href="https://www.instagram.com/terriblecomposer/">Instagram</a></li> 
+          <li><a href="https://www.linkedin.com/in/planetabhi/">LinkedIn</a></li>                         
+          <li><a href="https://twitter.com/planetabhi">Twitter</a></li>
+          <li><a href="https://www.youtube.com/@Terrible_">YouTube</a></li>
+        </ul>
+      </div>
+      
+      <div class="col-3">
+      </div>
+      
+      <div class="col-3">
+      </div>
+    </div>
+  </div>
+</section>
+</div>
+`
